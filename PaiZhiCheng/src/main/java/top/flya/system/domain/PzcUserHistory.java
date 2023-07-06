30aff5a797042e746c1b65712b952383f9842160
fl1906 <1906249647@qq.com>
2023-07-06 16:14:15 +0800

    feat 用户操作历史CRUD

---
+++ PaiZhiCheng/src/main/java/top/flya/system/domain/PzcUserHistory.java
@@ -0,0 +1,44 @@
+package top.flya.system.domain;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import top.flya.common.core.domain.BaseEntity;
+
+
+/**
+ * 用户操作历史记录对象 pzc_user_history
+ *
+ * @author ruoyi
+ * @date 2023-07-06
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("pzc_user_history")
+public class PzcUserHistory extends BaseEntity {
+
+    private static final long serialVersionUID=1L;
+
+    /**
+     *
+     */
+    @TableId(value = "history_id",type = IdType.AUTO)
+    private Long historyId;
+    /**
+     * 关联用户Id
+     */
+    private Long userId;
+    /**
+     * 关联活动Id
+     */
+    private Long activityId;
+    /**
+     * 操作类型
+     */
+    private Long type;
+    /**
+     * 信息
+     */
+    private String message;
+
+}
