57bd513f15f3cfebcce36928714d0cbdfdce7d84
fl1906 <1906249647@qq.com>
2023-07-27 22:14:54 +0800

    feat 官方消息建表以及生成  7/10

---
+++ PaiZhiCheng/src/main/java/top/flya/system/domain/PzcOfficial.java
@@ -0,0 +1,54 @@
+package top.flya.system.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import top.flya.common.core.domain.BaseEntity;
+
+
+/**
+ * 官方消息对象 pzc_official
+ *
+ * @author ruoyi
+ * @date 2023-07-27
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("pzc_official")
+public class PzcOfficial extends BaseEntity {
+
+    private static final long serialVersionUID=1L;
+
+    /**
+     * ID
+     */
+    @TableId(value = "official_id",type = IdType.AUTO)
+    private Long officialId;
+    /**
+     * 来自谁的消息
+     */
+    private Long fromUserId;
+    /**
+     * 标题
+     */
+    private String title;
+    /**
+     * 主体消息
+     */
+    private String content;
+    /**
+     * 是否已读
+     */
+    private Long read;
+    /**
+     * 来自那场组队的消息
+     */
+    private Long groupId;
+    /**
+     * 来自那场活动的消息
+     */
+    private Long activityId;
+
+}
---
+++ PaiZhiCheng/src/main/java/top/flya/system/domain/bo/PzcOfficialBo.java
@@ -0,0 +1,67 @@
+package top.flya.system.domain.bo;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import top.flya.common.core.domain.BaseEntity;
+import top.flya.common.core.validate.AddGroup;
+import top.flya.common.core.validate.EditGroup;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+
+/**
+ * 官方消息业务对象 pzc_official
+ *
+ * @author ruoyi
+ * @date 2023-07-27
+ */
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class PzcOfficialBo extends BaseEntity {
+
+    /**
+     * ID
+     */
+    @NotNull(message = "ID不能为空", groups = { EditGroup.class })
+    private Long officialId;
+
+    /**
+     * 来自谁的消息
+     */
+    @NotNull(message = "来自谁的消息不能为空", groups = { AddGroup.class, EditGroup.class })
+    private Long fromUserId;
+
+    /**
+     * 标题
+     */
+    @NotBlank(message = "标题不能为空", groups = { AddGroup.class, EditGroup.class })
+    private String title;
+
+    /**
+     * 主体消息
+     */
+    @NotBlank(message = "主体消息不能为空", groups = { AddGroup.class, EditGroup.class })
+    private String content;
+
+    /**
+     * 是否已读
+     */
+    @NotNull(message = "是否已读不能为空", groups = { AddGroup.class, EditGroup.class })
+    private Long read;
+
+    /**
+     * 来自那场组队的消息
+     */
+    @NotNull(message = "来自那场组队的消息不能为空", groups = { AddGroup.class, EditGroup.class })
+    private Long groupId;
+
+    /**
+     * 来自那场活动的消息
+     */
+    @NotNull(message = "来自那场活动的消息不能为空", groups = { AddGroup.class, EditGroup.class })
+    private Long activityId;
+
+
+}
